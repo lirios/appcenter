2e03903041fa6910573e7b6a92991d8ead46ec38
Michael Spencer <sonrisesoftware@gmail.com>
2016-01-20 18:32:14 -0600

    Switch to using resource files

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,28 @@
 #include <QDebug>
 #include <QIcon>
 #include <QDir>
+#include <KQuickConfig>
 
 #define TR(x) QT_TRANSLATE_NOOP("Command line parser", QStringLiteral(x))
 
 int main(int argc, char *argv[])
 {
-        QGuiApplication app(argc, argv);
-        app.setApplicationName("App Center");
-        app.setOrganizationDomain("papyros.io");
-        app.setOrganizationName("Papyros");
+    QGuiApplication app(argc, argv);
+    app.setApplicationName("App Center");
+    app.setOrganizationDomain("papyros.io");
+    app.setOrganizationName("Papyros");
 
-        #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
-            app.setDesktopFileName("io.papyros.AppCenter.desktop");
-        #endif
+#if QT_VERSION >= QT_VERSION_CHECK(5, 7, 0)
+    app.setDesktopFileName("io.papyros.AppCenter.desktop");
+#endif
 
-        // Set the X11 WML_CLASS so X11 desktops can find the desktop file
-        qputenv("RESOURCE_NAME", "io.papyros.AppCenter");
+    // Set the X11 WML_CLASS so X11 desktops can find the desktop file
+    qputenv("RESOURCE_NAME", "io.papyros.AppCenter");
 
-        // TODO: Figure out why this is necessary
-        QIcon::setThemeName("Paper");
+    // TODO: Figure out why this is necessary
+    QIcon::setThemeName("Paper");
 
-        QString fileName = QStandardPaths::locate(QStandardPaths::GenericDataLocation,
-                                                  QStringLiteral("papyros-appcenter/app/main.qml"));
+    QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc://qml/main.qml")));
 
-        if (fileName.isEmpty()) {
-                qWarning() << "Failed to find the main QML file, aborting.";
-                return 1;
-        }
-
-        QQmlApplicationEngine engine(fileName);
-
-        return app.exec();
+    return app.exec();
 }
