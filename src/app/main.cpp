b959a179fa93196f0f10ddbf77641444d57b9a96
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2019-01-05 21:51:44 +0100

    Setup translations

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -28,7 +28,47 @@
 #include <QDebug>
 #include <QIcon>
 #include <QDir>
+#include <QTranslator>
 #include <QtQuickControls2/QQuickStyle>
+
+static void loadQtTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Load Qt translations
+    QTranslator *qtTranslator = new QTranslator(QCoreApplication::instance());
+    if (qtTranslator->load(QStringLiteral("qt_%1").arg(locale), QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        qApp->installTranslator(qtTranslator);
+    } else {
+        delete qtTranslator;
+    }
+#endif
+}
+
+static void loadAppTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Find the translations directory
+    const QString path = QLatin1String("liri-appcenter/translations");
+    const QString translationsDir =
+        QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                               path,
+                               QStandardPaths::LocateDirectory);
+
+    // Load shell translations
+    QTranslator *appTranslator = new QTranslator(QCoreApplication::instance());
+    if (appTranslator->load(QStringLiteral("%1/appcenter_%3").arg(translationsDir, locale))) {
+        QCoreApplication::installTranslator(appTranslator);
+    } else if (locale == QLatin1String("C") ||
+                locale.startsWith(QLatin1String("en"))) {
+        // English is the default, it's translated anyway
+        delete appTranslator;
+    }
+#endif
+}
 
 int main(int argc, char *argv[])
 {
@@ -44,6 +84,10 @@ int main(int argc, char *argv[])
     app.setDesktopFileName(QLatin1String("io.liri.AppCenter.desktop"));
 
     QQuickStyle::setStyle(QLatin1String("Material"));
+
+    // Load translations
+    loadQtTranslations();
+    loadAppTranslations();
 
     QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
--- src/notifier/main.cpp
+++ src/notifier/main.cpp
@@ -22,8 +22,51 @@
  ***************************************************************************/
 
 #include <QCoreApplication>
+#include <QLocale>
+#include <QLibraryInfo>
+#include <QStandardPaths>
+#include <QTranslator>
 
 #include "updatenotifier.h"
+
+static void loadQtTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Load Qt translations
+    QTranslator *qtTranslator = new QTranslator(QCoreApplication::instance());
+    if (qtTranslator->load(QStringLiteral("qt_%1").arg(locale), QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        qApp->installTranslator(qtTranslator);
+    } else {
+        delete qtTranslator;
+    }
+#endif
+}
+
+static void loadAppTranslations()
+{
+#ifndef QT_NO_TRANSLATION
+    QString locale = QLocale::system().name();
+
+    // Find the translations directory
+    const QString path = QLatin1String("liri-appcenter/translations");
+    const QString translationsDir =
+        QStandardPaths::locate(QStandardPaths::GenericDataLocation,
+                               path,
+                               QStandardPaths::LocateDirectory);
+
+    // Load shell translations
+    QTranslator *appTranslator = new QTranslator(QCoreApplication::instance());
+    if (appTranslator->load(QStringLiteral("%1/notifier_%3").arg(translationsDir, locale))) {
+        QCoreApplication::installTranslator(appTranslator);
+    } else if (locale == QLatin1String("C") ||
+                locale.startsWith(QLatin1String("en"))) {
+        // English is the default, it's translated anyway
+        delete appTranslator;
+    }
+#endif
+}
 
 int main(int argc, char *argv[])
 {
@@ -31,6 +74,10 @@ int main(int argc, char *argv[])
     app.setApplicationName(QLatin1String("App Center"));
     app.setOrganizationDomain(QLatin1String("liri.io"));
     app.setOrganizationName(QLatin1String("Liri"));
+
+    // Load translations
+    loadQtTranslations();
+    loadAppTranslations();
 
     UpdateNotifier notifier;
     notifier.checkForUpdates();
