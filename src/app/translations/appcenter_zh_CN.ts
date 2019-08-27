4028d2b146ed54abd2ab7d062abcf1c35aea5d73
Liri CI <ci@liri.io>
2019-08-27 02:45:33 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ src/app/translations/appcenter_zh_CN.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>AddSourceDialog</name>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="32"/>
+        <source>Add Source</source>
+        <translation>添加源</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="47"/>
+        <source>Failed to retrieve source</source>
+        <translation>源检索失败</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="55"/>
+        <source>URL</source>
+        <translation>链接</translation>
+    </message>
+</context>
+<context>
+    <name>AllAppsTab</name>
+    <message>
+        <location filename="../qml/AllAppsTab.qml" line="30"/>
+        <source>All Apps</source>
+        <translation>所有应用</translation>
+    </message>
+</context>
+<context>
+    <name>AppPage</name>
+    <message>
+        <location filename="../qml/AppPage.qml" line="130"/>
+        <source>Install</source>
+        <translation>安装</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="144"/>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="160"/>
+        <source>Update</source>
+        <translation>更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="175"/>
+        <source>Launch</source>
+        <translation>启动</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="203"/>
+        <source>Website</source>
+        <translation>网站</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="249"/>
+        <source>Details</source>
+        <translation>细节</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="275"/>
+        <source>Localized in your language</source>
+        <translation>已本地化您的语言</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="286"/>
+        <source>Documentation</source>
+        <translation>文档</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="297"/>
+        <source>Release activity</source>
+        <translation>活动发布</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="308"/>
+        <source>System integration</source>
+        <translation>系统整合</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="319"/>
+        <source>Sandboxed</source>
+        <translation>沙盒化</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="338"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="348"/>
+        <source>Updated</source>
+        <translation>已更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="352"/>
+        <location filename="../qml/AppPage.qml" line="362"/>
+        <location filename="../qml/AppPage.qml" line="372"/>
+        <location filename="../qml/AppPage.qml" line="383"/>
+        <source>Unknown</source>
+        <translation>位置</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="358"/>
+        <source>Category</source>
+        <translation>分类</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="368"/>
+        <source>License</source>
+        <translation>授权许可</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="379"/>
+        <source>Source</source>
+        <translation>源</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Installed size</source>
+        <translation>安装大小</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Download size</source>
+        <translation>下载大小</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="402"/>
+        <source>Reviews</source>
+        <translation>评论</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="409"/>
+        <source>Sorry, but reviews are not implemented yet.</source>
+        <translation>抱歉，目前无法评论。</translation>
+    </message>
+</context>
+<context>
+    <name>InstalledAppsTab</name>
+    <message>
+        <location filename="../qml/InstalledAppsTab.qml" line="31"/>
+        <source>Installed Apps</source>
+        <translation>已安装应用</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesTab</name>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="29"/>
+        <source>Sources</source>
+        <translation>源</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="36"/>
+        <source>Edit %1</source>
+        <translation>编辑 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="74"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="82"/>
+        <source>Remove</source>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="86"/>
+        <source>%1 (%2)</source>
+        <translation>%1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesTab</name>
+    <message>
+        <location filename="../qml/UpdatesTab.qml" line="31"/>
+        <source>Updates</source>
+        <translation>更新</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="32"/>
+        <location filename="../qml/main.qml" line="59"/>
+        <source>App Center</source>
+        <translation>应用中心</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="64"/>
+        <source>Add source</source>
+        <translation>添加源</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <source>Add a new source</source>
+        <translation>添加一个新源</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="70"/>
+        <location filename="../qml/main.qml" line="71"/>
+        <source>Check for updates</source>
+        <translation>检查更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="76"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="77"/>
+        <source>Search apps</source>
+        <translation>搜索应用</translation>
+    </message>
+</context>
+</TS>
---
+++ src/notifier/translations/notifier_zh_CN.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<context>
+    <name>UpdateNotifier</name>
+    <message>
+        <location filename="../updatenotifier.cpp" line="50"/>
+        <source>Updates available</source>
+        <translation>有可用更新</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="52"/>
+        <source>Install updates</source>
+        <translation>安装更新</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="62"/>
+        <source>No updates are available</source>
+        <translation>无可用更新</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="64"/>
+        <source>%1 is ready to update</source>
+        <translation>%1更新准备就绪</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="66"/>
+        <source>%1 and %2 are ready to update</source>
+        <translation>%1和%2更新准备就绪</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="68"/>
+        <source>%1, %2, and one other app are ready to update</source>
+        <translation>%1，%2和另一个应用更新准备就绪</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="71"/>
+        <source>%1, %2, and %3 other apps are ready to update</source>
+        <translation>%1，%2和%3以及其他应用更新准备就绪</translation>
+    </message>
+</context>
+</TS>
