053a1ca0dc8805d20a90f4ba0588205440431d80
Liri CI <ci@liri.io>
2020-04-19 22:03:24 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/appcenter.ts
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AddSourceDialog</name>
+    <message>
+        <location filename="../src/app/qml/AddSourceDialog.qml" line="32"/>
+        <source>Add Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AddSourceDialog.qml" line="47"/>
+        <source>Failed to retrieve source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AddSourceDialog.qml" line="55"/>
+        <source>URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AllAppsTab</name>
+    <message>
+        <location filename="../src/app/qml/AllAppsTab.qml" line="30"/>
+        <source>All Apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AppPage</name>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="130"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="144"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="160"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="175"/>
+        <source>Launch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="203"/>
+        <source>Website</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="249"/>
+        <source>Details</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="275"/>
+        <source>Localized in your language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="286"/>
+        <source>Documentation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="297"/>
+        <source>Release activity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="308"/>
+        <source>System integration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="319"/>
+        <source>Sandboxed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="338"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="348"/>
+        <source>Updated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="352"/>
+        <location filename="../src/app/qml/AppPage.qml" line="362"/>
+        <location filename="../src/app/qml/AppPage.qml" line="372"/>
+        <location filename="../src/app/qml/AppPage.qml" line="383"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="358"/>
+        <source>Category</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="368"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="379"/>
+        <source>Source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="390"/>
+        <source>Installed size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="390"/>
+        <source>Download size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="402"/>
+        <source>Reviews</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/AppPage.qml" line="409"/>
+        <source>Sorry, but reviews are not implemented yet.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InstalledAppsTab</name>
+    <message>
+        <location filename="../src/app/qml/InstalledAppsTab.qml" line="31"/>
+        <source>Installed Apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SourcesTab</name>
+    <message>
+        <location filename="../src/app/qml/SourcesTab.qml" line="29"/>
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SourcesTab.qml" line="36"/>
+        <source>Edit %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SourcesTab.qml" line="74"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SourcesTab.qml" line="82"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/SourcesTab.qml" line="86"/>
+        <source>%1 (%2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesTab</name>
+    <message>
+        <location filename="../src/app/qml/UpdatesTab.qml" line="31"/>
+        <source>Updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="32"/>
+        <location filename="../src/app/qml/main.qml" line="59"/>
+        <source>App Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="64"/>
+        <source>Add source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="65"/>
+        <source>Add a new source</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="70"/>
+        <location filename="../src/app/qml/main.qml" line="71"/>
+        <source>Check for updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="76"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/app/qml/main.qml" line="77"/>
+        <source>Search apps</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/notifier.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>UpdateNotifier</name>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="50"/>
+        <source>Updates available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="52"/>
+        <source>Install updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="62"/>
+        <source>No updates are available</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="64"/>
+        <source>%1 is ready to update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="66"/>
+        <source>%1 and %2 are ready to update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="68"/>
+        <source>%1, %2, and one other app are ready to update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/notifier/updatenotifier.cpp" line="71"/>
+        <source>%1, %2, and %3 other apps are ready to update</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
