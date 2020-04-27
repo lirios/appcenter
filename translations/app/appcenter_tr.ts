660a843ef32818bfde1340d8527c49016b843a7d
Liri CI <ci@liri.io>
2020-04-27 10:24:59 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/app/appcenter_tr.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.1">
+<context>
+    <name>AddSourceDialog</name>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="32"/>
+        <source>Add Source</source>
+        <translation>Kaynak Ekle</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="47"/>
+        <source>Failed to retrieve source</source>
+        <translation>Kaynak getirilemedi</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="55"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+</context>
+<context>
+    <name>AllAppsTab</name>
+    <message>
+        <location filename="../qml/AllAppsTab.qml" line="30"/>
+        <source>All Apps</source>
+        <translation>Tüm Uygulamalar</translation>
+    </message>
+</context>
+<context>
+    <name>AppPage</name>
+    <message>
+        <location filename="../qml/AppPage.qml" line="130"/>
+        <source>Install</source>
+        <translation>Kur</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="144"/>
+        <source>Remove</source>
+        <translation>Kaldır</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="160"/>
+        <source>Update</source>
+        <translation>Güncelle</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="175"/>
+        <source>Launch</source>
+        <translation>Başlat</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="203"/>
+        <source>Website</source>
+        <translation>Web Sitesi</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="249"/>
+        <source>Details</source>
+        <translation>Ayrıntılar</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="275"/>
+        <source>Localized in your language</source>
+        <translation>Dilinize çevrilmiş</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="286"/>
+        <source>Documentation</source>
+        <translation>Belgeleme</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="297"/>
+        <source>Release activity</source>
+        <translation>Dağıtım etkinliği</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="308"/>
+        <source>System integration</source>
+        <translation>Sistem tümleşimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="319"/>
+        <source>Sandboxed</source>
+        <translation>Yalıtılmış</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="338"/>
+        <source>Version</source>
+        <translation>Sürüm</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="348"/>
+        <source>Updated</source>
+        <translation>Güncelleme</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="352"/>
+        <location filename="../qml/AppPage.qml" line="362"/>
+        <location filename="../qml/AppPage.qml" line="372"/>
+        <location filename="../qml/AppPage.qml" line="383"/>
+        <source>Unknown</source>
+        <translation>Bilinmiyor</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="358"/>
+        <source>Category</source>
+        <translation>Kategori</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="368"/>
+        <source>License</source>
+        <translation>Lisans</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="379"/>
+        <source>Source</source>
+        <translation>Kaynak</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Installed size</source>
+        <translation>Kurulan boyut</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Download size</source>
+        <translation>İndirme boyutu</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="402"/>
+        <source>Reviews</source>
+        <translation>İncelemeler</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="409"/>
+        <source>Sorry, but reviews are not implemented yet.</source>
+        <translation>Üzgünüz ama incelemeler henüz gerçeklenmedi.</translation>
+    </message>
+</context>
+<context>
+    <name>InstalledAppsTab</name>
+    <message>
+        <location filename="../qml/InstalledAppsTab.qml" line="31"/>
+        <source>Installed Apps</source>
+        <translation>Kurulan Uygulamalar</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesTab</name>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="29"/>
+        <source>Sources</source>
+        <translation>Kaynaklar</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="36"/>
+        <source>Edit %1</source>
+        <translation>Düzenle: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="74"/>
+        <source>Edit</source>
+        <translation>Düzenle</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="82"/>
+        <source>Remove</source>
+        <translation>Kaldır</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="86"/>
+        <source>%1 (%2)</source>
+        <translation>%1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesTab</name>
+    <message>
+        <location filename="../qml/UpdatesTab.qml" line="31"/>
+        <source>Updates</source>
+        <translation>Güncellemeler</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="32"/>
+        <location filename="../qml/main.qml" line="59"/>
+        <source>App Center</source>
+        <translation>Uygulama Merkezi</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="64"/>
+        <source>Add source</source>
+        <translation>Kaynak ekle</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <source>Add a new source</source>
+        <translation>Yeni kaynak ekle</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="70"/>
+        <location filename="../qml/main.qml" line="71"/>
+        <source>Check for updates</source>
+        <translation>Güncellemeleri denetle</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="76"/>
+        <source>Search</source>
+        <translation>Ara</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="77"/>
+        <source>Search apps</source>
+        <translation>Uygulama ara</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/notifier/notifier_tr.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.1">
+<context>
+    <name>UpdateNotifier</name>
+    <message>
+        <location filename="../updatenotifier.cpp" line="50"/>
+        <source>Updates available</source>
+        <translation>Güncellemeler var</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="52"/>
+        <source>Install updates</source>
+        <translation>Güncellemeleri kur</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="62"/>
+        <source>No updates are available</source>
+        <translation>Güncelleme yok</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="64"/>
+        <source>%1 is ready to update</source>
+        <translation>%1 güncellemeye hazır</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="66"/>
+        <source>%1 and %2 are ready to update</source>
+        <translation>%1 ve %2 güncellemeye hazır</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="68"/>
+        <source>%1, %2, and one other app are ready to update</source>
+        <translation>%1, %2 ve diğer bir uygulama güncellemeye hazır</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="71"/>
+        <source>%1, %2, and %3 other apps are ready to update</source>
+        <translation>%1, %2 ve diğer %3 uygulama güncellemeye hazır</translation>
+    </message>
+</context>
+</TS>
