ab3ab96b6acb7c28b631d65f950a0c572cf933b2
Liri CI <ci@liri.io>
2020-05-11 00:10:53 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/notifier/notifier_it_IT.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="it_IT" version="2.1">
+<context>
+    <name>UpdateNotifier</name>
+    <message>
+        <location filename="../updatenotifier.cpp" line="50"/>
+        <source>Updates available</source>
+        <translation>Aggiornamenti disponibili</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="52"/>
+        <source>Install updates</source>
+        <translation>Installa aggiornamenti</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="62"/>
+        <source>No updates are available</source>
+        <translation>Nessun aggiornamento disponibile</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="64"/>
+        <source>%1 is ready to update</source>
+        <translation>%1 è pronto per l&apos;aggiornamento</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="66"/>
+        <source>%1 and %2 are ready to update</source>
+        <translation>%1 e %2 sono pronti per l&apos;aggiornamento</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="68"/>
+        <source>%1, %2, and one other app are ready to update</source>
+        <translation>%1, %2 e un&apos;altra applicazione sono pronte per l&apos;aggiornamento</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="71"/>
+        <source>%1, %2, and %3 other apps are ready to update</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
