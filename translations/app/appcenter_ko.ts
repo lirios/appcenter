8432d26f34fc40b9285cf7a59a411dc3a3f62b38
Liri CI <ci@liri.io>
2020-05-15 00:10:39 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/app/appcenter_ko.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ko" version="2.1">
+<context>
+    <name>AddSourceDialog</name>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="32"/>
+        <source>Add Source</source>
+        <translation>소스 추가하기</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="47"/>
+        <source>Failed to retrieve source</source>
+        <translation>소스 가져오기 실패</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="55"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+</context>
+<context>
+    <name>AllAppsTab</name>
+    <message>
+        <location filename="../qml/AllAppsTab.qml" line="30"/>
+        <source>All Apps</source>
+        <translation>모든 앱</translation>
+    </message>
+</context>
+<context>
+    <name>AppPage</name>
+    <message>
+        <location filename="../qml/AppPage.qml" line="130"/>
+        <source>Install</source>
+        <translation>설치</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="144"/>
+        <source>Remove</source>
+        <translation>제거</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="160"/>
+        <source>Update</source>
+        <translation>업데이트</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="175"/>
+        <source>Launch</source>
+        <translation>실행</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="203"/>
+        <source>Website</source>
+        <translation>웹사이트</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="249"/>
+        <source>Details</source>
+        <translation>자세히</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="275"/>
+        <source>Localized in your language</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="286"/>
+        <source>Documentation</source>
+        <translation>문서</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="297"/>
+        <source>Release activity</source>
+        <translation>배포 내역</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="308"/>
+        <source>System integration</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="319"/>
+        <source>Sandboxed</source>
+        <translation>샌드박스화 됨</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="338"/>
+        <source>Version</source>
+        <translation>버전</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="348"/>
+        <source>Updated</source>
+        <translation>업데이트 됨</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="352"/>
+        <location filename="../qml/AppPage.qml" line="362"/>
+        <location filename="../qml/AppPage.qml" line="372"/>
+        <location filename="../qml/AppPage.qml" line="383"/>
+        <source>Unknown</source>
+        <translation>알 수 없음</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="358"/>
+        <source>Category</source>
+        <translation>분류</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="368"/>
+        <source>License</source>
+        <translation>라이센스</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="379"/>
+        <source>Source</source>
+        <translation>소스</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Installed size</source>
+        <translation>설치 된 크기</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Download size</source>
+        <translation>다운로드 크기</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="402"/>
+        <source>Reviews</source>
+        <translation>리뷰</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="409"/>
+        <source>Sorry, but reviews are not implemented yet.</source>
+        <translation>죄송합니다, 리뷰는 현재 구현되지 않았습니다.</translation>
+    </message>
+</context>
+<context>
+    <name>InstalledAppsTab</name>
+    <message>
+        <location filename="../qml/InstalledAppsTab.qml" line="31"/>
+        <source>Installed Apps</source>
+        <translation>설치 된 앱</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesTab</name>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="29"/>
+        <source>Sources</source>
+        <translation>소스</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="36"/>
+        <source>Edit %1</source>
+        <translation>%1 수정</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="74"/>
+        <source>Edit</source>
+        <translation>수정</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="82"/>
+        <source>Remove</source>
+        <translation>제거</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="86"/>
+        <source>%1 (%2)</source>
+        <translation>%1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesTab</name>
+    <message>
+        <location filename="../qml/UpdatesTab.qml" line="31"/>
+        <source>Updates</source>
+        <translation>업데이트</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="32"/>
+        <location filename="../qml/main.qml" line="59"/>
+        <source>App Center</source>
+        <translation>앱 센터</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="64"/>
+        <source>Add source</source>
+        <translation>소스 추가하기</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <source>Add a new source</source>
+        <translation>새 소스 추가하기</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="70"/>
+        <location filename="../qml/main.qml" line="71"/>
+        <source>Check for updates</source>
+        <translation>업데이트 확인</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="76"/>
+        <source>Search</source>
+        <translation>검색</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="77"/>
+        <source>Search apps</source>
+        <translation>앱 검색</translation>
+    </message>
+</context>
+</TS>
