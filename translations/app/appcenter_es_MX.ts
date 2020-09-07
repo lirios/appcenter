801144abe5a0de4968624a86847f122f4c5c3489
Liri CI <ci@liri.io>
2020-09-07 00:29:38 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/app/appcenter_es_MX.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_MX" version="2.1">
+<context>
+    <name>AddSourceDialog</name>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="32"/>
+        <source>Add Source</source>
+        <translation>Agregue una fuente</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="47"/>
+        <source>Failed to retrieve source</source>
+        <translation>No se pudo recuperar la fuente</translation>
+    </message>
+    <message>
+        <location filename="../qml/AddSourceDialog.qml" line="55"/>
+        <source>URL</source>
+        <translation>URL</translation>
+    </message>
+</context>
+<context>
+    <name>AllAppsTab</name>
+    <message>
+        <location filename="../qml/AllAppsTab.qml" line="30"/>
+        <source>All Apps</source>
+        <translation>Todas las aplicaciones</translation>
+    </message>
+</context>
+<context>
+    <name>AppPage</name>
+    <message>
+        <location filename="../qml/AppPage.qml" line="130"/>
+        <source>Install</source>
+        <translation>Instalar</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="144"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="160"/>
+        <source>Update</source>
+        <translation>Actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="175"/>
+        <source>Launch</source>
+        <translation>Lanzar</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="203"/>
+        <source>Website</source>
+        <translation>Sitio web</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="249"/>
+        <source>Details</source>
+        <translation>Detalles</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="275"/>
+        <source>Localized in your language</source>
+        <translation>Localizado en tu idioma</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="286"/>
+        <source>Documentation</source>
+        <translation>Documentación </translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="297"/>
+        <source>Release activity</source>
+        <translation>Actividad de lanzamiento</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="308"/>
+        <source>System integration</source>
+        <translation>Integración de sistema</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="319"/>
+        <source>Sandboxed</source>
+        <translation>Modo seguro</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="338"/>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="348"/>
+        <source>Updated</source>
+        <translation>Actualizado</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="352"/>
+        <location filename="../qml/AppPage.qml" line="362"/>
+        <location filename="../qml/AppPage.qml" line="372"/>
+        <location filename="../qml/AppPage.qml" line="383"/>
+        <source>Unknown</source>
+        <translation>Desconocido</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="358"/>
+        <source>Category</source>
+        <translation>Categoría</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="368"/>
+        <source>License</source>
+        <translation>Licencia</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="379"/>
+        <source>Source</source>
+        <translation>Fuente</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Installed size</source>
+        <translation>Tamaño instalado</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="390"/>
+        <source>Download size</source>
+        <translation>Tamaño descargado</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="402"/>
+        <source>Reviews</source>
+        <translation>Reseñas</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppPage.qml" line="409"/>
+        <source>Sorry, but reviews are not implemented yet.</source>
+        <translation>Lo siento, las reseñas aun no están implementadas</translation>
+    </message>
+</context>
+<context>
+    <name>InstalledAppsTab</name>
+    <message>
+        <location filename="../qml/InstalledAppsTab.qml" line="31"/>
+        <source>Installed Apps</source>
+        <translation>Aplicaciones instaladas</translation>
+    </message>
+</context>
+<context>
+    <name>SourcesTab</name>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="29"/>
+        <source>Sources</source>
+        <translation>Fuentes</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="36"/>
+        <source>Edit %1</source>
+        <translation>Editar %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="74"/>
+        <source>Edit</source>
+        <translation>Editar</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="82"/>
+        <source>Remove</source>
+        <translation>Eliminar</translation>
+    </message>
+    <message>
+        <location filename="../qml/SourcesTab.qml" line="86"/>
+        <source>%1 (%2)</source>
+        <translation>%1 (%2)</translation>
+    </message>
+</context>
+<context>
+    <name>UpdatesTab</name>
+    <message>
+        <location filename="../qml/UpdatesTab.qml" line="31"/>
+        <source>Updates</source>
+        <translation>Actualizado</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="32"/>
+        <location filename="../qml/main.qml" line="59"/>
+        <source>App Center</source>
+        <translation>Centro de aplicaciones</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="64"/>
+        <source>Add source</source>
+        <translation>Agregue una fuente</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="65"/>
+        <source>Add a new source</source>
+        <translation>Agregar una nueva fuente</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="70"/>
+        <location filename="../qml/main.qml" line="71"/>
+        <source>Check for updates</source>
+        <translation>Buscar actualizaciones</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="76"/>
+        <source>Search</source>
+        <translation>Buscar</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="77"/>
+        <source>Search apps</source>
+        <translation>Buscar aplicaciones</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/notifier/notifier_es_MX.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="es_MX" version="2.1">
+<context>
+    <name>UpdateNotifier</name>
+    <message>
+        <location filename="../updatenotifier.cpp" line="50"/>
+        <source>Updates available</source>
+        <translation>Actualizaciones disponibles</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="52"/>
+        <source>Install updates</source>
+        <translation>Instalar actualizaciones</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="62"/>
+        <source>No updates are available</source>
+        <translation>No hay actualizaciones disponibles</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="64"/>
+        <source>%1 is ready to update</source>
+        <translation>%1 esta listo para actualizarse</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="66"/>
+        <source>%1 and %2 are ready to update</source>
+        <translation>%1 y %2 están listo para actualizarse</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="68"/>
+        <source>%1, %2, and one other app are ready to update</source>
+        <translation>%1, %2 y otra aplicación están listas para actualizarse</translation>
+    </message>
+    <message>
+        <location filename="../updatenotifier.cpp" line="71"/>
+        <source>%1, %2, and %3 other apps are ready to update</source>
+        <translation>%1, %2 y %3 otras aplicaciones están listas para actualizarse</translation>
+    </message>
+</context>
+</TS>
