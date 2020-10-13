8890d57678c763778e4ac30fc84e420c872fe3b2
Liri CI <ci@liri.io>
2020-10-13 00:33:57 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/notifier.ts
+++ translations/notifier.ts
@@ -4,43 +4,43 @@
 <context>
     <name>UpdateNotifier</name>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="60"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="61"/>
         <source>Updates available</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="62"/>
         <location filename="../src/notifier/updatenotifier.cpp" line="63"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="64"/>
         <source>Install updates</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="62"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="63"/>
         <source>Not now</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="82"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="83"/>
         <source>No updates are available</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="84"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="85"/>
         <source>%1 is ready to update</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="86"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="87"/>
         <source>%1 and %2 are ready to update</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="88"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="89"/>
         <source>%1, %2, and one other app are ready to update</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/notifier/updatenotifier.cpp" line="91"/>
+        <location filename="../src/notifier/updatenotifier.cpp" line="92"/>
         <source>%1, %2, and %3 other apps are ready to update</source>
         <translation type="unfinished"></translation>
     </message>
